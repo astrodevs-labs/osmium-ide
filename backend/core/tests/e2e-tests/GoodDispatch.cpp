a922b596f66d1284d9d3c3e5d0aa7842fd025a4d
0xMemoryGrinder <alexjublot@gmail.com>
2023-04-06 01:39:44 +0200

    tests(backend/core): added e2e tests

--- backend/core/tests/e2e-tests/GoodDispatch.cpp
+++ backend/core/tests/e2e-tests/GoodDispatch.cpp
@@ -7,10 +7,109 @@
 #include "ModulesMediator.hpp"
 #include "Core.hpp"
 
+core::tests::ModulesMediator::Type type;
+
 TEST(GoodDispatch, SimpleString)
 {
     core::tests::ModulesMediator::setType(core::tests::ModulesMediator::Type::STRING);
+    core::tests::ModulesMediator::setup();
     auto core = std::make_shared<core::Core>();
 
-    core->loadSoFile("core/tests/libbackend-core-tests-input-module.so");
+    core->loadSoFile("core/tests/backend-core-tests-input-module-string.so");
+    core->emitMessage(std::make_shared<std::string>("Hello World!"), "test").wait();
+    core::tests::ModulesMediator::parseReceived();
+    auto received = core::tests::ModulesMediator::getReceived("InputModule");
+    ASSERT_EQ(received.size(), 1);
+    ASSERT_STREQ(std::any_cast<std::string>(received[0]).c_str(), "Hello World!");
+    core::tests::ModulesMediator::readerTeardown();
+}
+
+TEST(GoodDispatch, SimpleInt)
+{
+    core::tests::ModulesMediator::setType(core::tests::ModulesMediator::Type::INT);
+    core::tests::ModulesMediator::setup();
+    auto core = std::make_shared<core::Core>();
+
+    core->loadSoFile("core/tests/backend-core-tests-input-module-int.so");
+    core->emitMessage(std::make_shared<int>(42), "test").wait();
+    core::tests::ModulesMediator::parseReceived();
+    auto received = core::tests::ModulesMediator::getReceived("InputModule");
+    ASSERT_EQ(received.size(), 1);
+    ASSERT_EQ(std::any_cast<int>(received[0]), 42);
+    core::tests::ModulesMediator::readerTeardown();
+}
+
+TEST(GoodDispatch, SimpleFloat)
+{
+    core::tests::ModulesMediator::setType(core::tests::ModulesMediator::Type::FLOAT);
+    core::tests::ModulesMediator::setup();
+    auto core = std::make_shared<core::Core>();
+
+    core->loadSoFile("core/tests/backend-core-tests-input-module-float.so");
+    core->emitMessage(std::make_shared<float>(42.42), "test").wait();
+    core::tests::ModulesMediator::parseReceived();
+    auto received = core::tests::ModulesMediator::getReceived("InputModule");
+    ASSERT_EQ(received.size(), 1);
+    ASSERT_FLOAT_EQ(std::any_cast<float>(received[0]), 42.42);
+    core::tests::ModulesMediator::readerTeardown();
+}
+
+TEST(GoodDispatch, SimpleDouble)
+{
+    core::tests::ModulesMediator::setType(core::tests::ModulesMediator::Type::DOUBLE);
+    core::tests::ModulesMediator::setup();
+    auto core = std::make_shared<core::Core>();
+
+    core->loadSoFile("core/tests/backend-core-tests-input-module-double.so");
+    core->emitMessage(std::make_shared<double>(42.42), "test").wait();
+    core::tests::ModulesMediator::parseReceived();
+    auto received = core::tests::ModulesMediator::getReceived("InputModule");
+    ASSERT_EQ(received.size(), 1);
+    ASSERT_DOUBLE_EQ(std::any_cast<double>(received[0]), 42.42);
+    core::tests::ModulesMediator::readerTeardown();
+}
+
+TEST(GoodDispatch, SimpleChar)
+{
+    core::tests::ModulesMediator::setType(core::tests::ModulesMediator::Type::CHAR);
+    core::tests::ModulesMediator::setup();
+    auto core = std::make_shared<core::Core>();
+
+    core->loadSoFile("core/tests/backend-core-tests-input-module-char.so");
+    core->emitMessage(std::make_shared<char>('a'), "test").wait();
+    core::tests::ModulesMediator::parseReceived();
+    auto received = core::tests::ModulesMediator::getReceived("InputModule");
+    ASSERT_EQ(received.size(), 1);
+    ASSERT_EQ(std::any_cast<char>(received[0]), 'a');
+    core::tests::ModulesMediator::readerTeardown();
+}
+
+TEST(GoodDispatch, SimpleBool)
+{
+    core::tests::ModulesMediator::setType(core::tests::ModulesMediator::Type::BOOL);
+    core::tests::ModulesMediator::setup();
+    auto core = std::make_shared<core::Core>();
+
+    core->loadSoFile("core/tests/backend-core-tests-input-module-bool.so");
+    core->emitMessage(std::make_shared<bool>(true), "test").wait();
+    core::tests::ModulesMediator::parseReceived();
+    auto received = core::tests::ModulesMediator::getReceived("InputModule");
+    ASSERT_EQ(received.size(), 1);
+    ASSERT_EQ(std::any_cast<bool>(received[0]), true);
+    core::tests::ModulesMediator::readerTeardown();
+}
+
+TEST(GoodDispatch, SimpleNull)
+{
+    core::tests::ModulesMediator::setType(core::tests::ModulesMediator::Type::BOOL);
+    core::tests::ModulesMediator::setup();
+    auto core = std::make_shared<core::Core>();
+
+    core->loadSoFile("core/tests/backend-core-tests-input-module-bool.so");
+    core->emitMessage(std::make_shared<bool>(nullptr), "test").wait();
+    core::tests::ModulesMediator::parseReceived();
+    auto received = core::tests::ModulesMediator::getReceived("InputModule");
+    ASSERT_EQ(received.size(), 1);
+    ASSERT_EQ(std::any_cast<bool>(received[0]), 0);
+    core::tests::ModulesMediator::readerTeardown();
 }
--- backend/core/tests/inputModule/src/symbols.cpp
+++ backend/core/tests/inputModule/src/symbols.cpp
@@ -14,10 +14,30 @@ extern "C" {
     }
 
     void *createModule(std::shared_ptr<core::MessagingInterface> msg) {
-        switch (core::tests::ModulesMediator::getType()) {
+        core::tests::ModulesMediator::Type type;
+#ifdef TYPE_STRING
+        type = core::tests::ModulesMediator::Type::STRING;
+#elif defined(TYPE_INT)
+        type = core::tests::ModulesMediator::Type::INT;
+#elif defined(TYPE_FLOAT)
+        type = core::tests::ModulesMediator::Type::FLOAT;
+#elif defined(TYPE_DOUBLE)
+        type = core::tests::ModulesMediator::Type::DOUBLE;
+#elif defined(TYPE_CHAR)
+        type = core::tests::ModulesMediator::Type::CHAR;
+#elif defined(TYPE_BOOL)
+        type = core::tests::ModulesMediator::Type::BOOL;
+#elif defined(TYPE_EMPTY)
+        type = core::tests::ModulesMediator::Type::EMPTY;
+#else
+        type = core::tests::ModulesMediator::Type::CUSTOM;
+#endif
+
+        switch (type) {
             case core::tests::ModulesMediator::Type::STRING:
                 return new core::tests::input::InputModule<std::string>(msg);
             case core::tests::ModulesMediator::Type::INT:
+                std::cerr << "INT" << std::endl;
                 return new core::tests::input::InputModule<int>(msg);
             case core::tests::ModulesMediator::Type::FLOAT:
                 return new core::tests::input::InputModule<float>(msg);
@@ -27,11 +47,12 @@ extern "C" {
                 return new core::tests::input::InputModule<char>(msg);
             case core::tests::ModulesMediator::Type::BOOL:
                 return new core::tests::input::InputModule<bool>(msg);
-            case core::tests::ModulesMediator::Type::EMPTY:
-                return new core::tests::input::InputModule<void>(msg);
+//            case core::tests::ModulesMediator::Type::EMPTY:
+//                return new core::tests::input::InputModule<void>(msg);
             // TODO: add custom type
             default:
-                return new core::tests::input::InputModule<void>(msg);
+//                return new core::tests::input::InputModule<void>(msg);
+                return nullptr;
         }
     }
 }
