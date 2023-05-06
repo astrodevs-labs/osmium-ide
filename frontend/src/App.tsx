2c6e5051b764998216aae5c6c768dbaaeb0650af
Théo Maringer <theo.maringer@epitech.eu>
2023-05-06 11:46:33 +0200

    feat(framework): adding tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,16 @@
-import './App.css';
 import Toolbar from './components/Toolbar/Toolbar';
 import { useState } from 'react';
 
 const App = () => {
   const [content, setContent] = useState<string>('');
   return (
-    <div className="container">
+    <div className="w-full h-screen">
       <Toolbar content={content} setContent={setContent} />
       <textarea
         onChange={(event) => {
           setContent(event.target.value);
         }}
-        className="textarea"
+        className="border-gray-900 text-xl w-full bg-gray-900 text-gray-300"
         value={content}
       ></textarea>
     </div>
