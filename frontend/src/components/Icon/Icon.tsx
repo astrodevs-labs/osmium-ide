7ebee9b51a052b3547d9a0a15a2a8c3284535320
Théo Maringer <theo.maringer@epitech.eu>
2023-05-09 18:01:39 +0200

    feat(frontend/components/basic): icon component

---
+++ frontend/src/components/Icon/Icon.tsx
@@ -0,0 +1,17 @@
+import { IconProps } from './Icon.types';
+
+const icons: any = {
+  'arrow-left': 'M512 64L64 512l448 448 64-64L192 512l384-384z',
+  'arrow-right': 'M512 960L960 512 512 64 448 128l384 384-384 384z',
+  'arrow-up': 'M960 512L512 64 64 512l128 128 192-192v576h256V448l192 192z',
+  'arrow-down': 'M64 512l448 448 448-448-64-64-384 384L128 448z',
+};
+const Icon = (props: IconProps) => {
+  return (
+    <svg width="22" height="22" viewBox="0 0 1024 1024">
+      <path d={icons[props.icon]}></path>
+    </svg>
+  );
+};
+
+export default Icon;
