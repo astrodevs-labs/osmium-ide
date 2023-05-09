524f7d33a8955c5631bf35554512e77c94736fe6
Théo Maringer <theo.maringer@epitech.eu>
2023-05-09 18:01:39 +0200

    feat(frontend/components/basic): icon component

---
+++ frontend/src/components/Components.ts
@@ -0,0 +1,7 @@
+import Button from './Button/Button';
+import CheckBox from './CheckBox/CheckBox';
+import Label from './Label/Label';
+import Toolbar from './Toolbar/Toolbar';
+import Icon from './Icon/Icon';
+
+export { Button, CheckBox, Label, Toolbar, Icon };
