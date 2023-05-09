4bcabab691500f997269e0c880eeaf53f33c0b45
EnergyCube <dev@energycube.fr>
2023-05-09 21:26:09 +0200

    fix(frontend/components/basic): card and reworked button/toolbar

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ButtonProps } from './Button.types';
 
-const Button: React.FC<ButtonProps> = ({ onClick, disabled, children }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, disabled, children, className }) => {
   return (
     <button
       className={
         'bg-blue-800 text-white font-bold py-2 px-4 rounded ' +
-        (disabled ? 'cursor-not-allowed opacity-50 bg-blue-500' : ' hover:bg-blue-600')
+        (disabled ? 'cursor-not-allowed opacity-50 bg-gray-500' : ' hover:bg-gray-600') +
+        ' ' +
+        className
       }
       onClick={onClick}
       disabled={disabled}
