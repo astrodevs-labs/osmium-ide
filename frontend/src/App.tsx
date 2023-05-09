1bbe63e0e424fc1789369611b79d16123b711e19
EnergyCube <dev@energycube.fr>
2023-05-09 21:26:09 +0200

    fix(frontend/components/basic): card and reworked button/toolbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
+import Toolbar from './components/Toolbar/Toolbar';
 import { useState } from 'react';
 import { Button, Icon, Folder } from './components/Components';
+import Card from './components/Card/Card';
 
 const data = {
   name: 'osmiumide',
@@ -39,13 +41,24 @@ const App = () => {
   const [content, setContent] = useState<string>('');
   return (
     <div className="w-full h-screen">
+      <Toolbar content={'test'} setContent={setContent} />
+      <br />
       <div className="grid grid-cols-3 gap-4">
         <Button>1</Button>
         <Button disabled>2</Button>
         <Icon icon={'arrow-up'} size={64} color="white" />
         <Icon icon={'arrow-down'} size={64} color="white" />
-        <Folder name={data.name} isFolder={data.isFolder} items={data.items} />
       </div>
+      <br />
+      <Card title="Card title" subtitle="Card subtitle">
+        <div>Card content</div>
+        <Folder name={data.name} isFolder={data.isFolder} items={data.items} />
+      </Card>
+      <br />
+      <Card title="Card title" subtitle="Card subtitle" disabled={true}>
+        <div>Card content</div>
+        <Folder name={data.name} isFolder={data.isFolder} items={data.items} />
+      </Card>
     </div>
   );
 };
