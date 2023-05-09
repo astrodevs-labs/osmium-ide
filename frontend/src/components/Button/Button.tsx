dde6e8591f1f35aea762bf97437497a8afe44fcc
Théo Maringer <theo.maringer@epitech.eu>
2023-05-09 10:14:30 +0200

    feat(frontend/components/basic): button with tailwind

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,17 +1,19 @@
-import './Button.css';
-import React from "react";
-import {ButtonProps} from "./Button.types";
+import React from 'react';
+import { ButtonProps } from './Button.types';
 
-const Button: React.FC<ButtonProps> = ({onClick, disabled, children}) => {
-    return (
-        <button
-            className={disabled ? "button disabled" : "button"}
-            onClick={onClick}
-            disabled={disabled}
-        >
-            {children}
-        </button>
-    );
+const Button: React.FC<ButtonProps> = ({ onClick, disabled, children }) => {
+  return (
+    <button
+      className={
+        'bg-blue-800 text-white font-bold py-2 px-4 rounded ' +
+        (disabled ? 'cursor-not-allowed opacity-50 bg-blue-500' : ' hover:bg-blue-600')
+      }
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
