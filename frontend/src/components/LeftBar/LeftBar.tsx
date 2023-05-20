b2337d18e0fcbdca18765ed44d6f13e975118cee
leon <maxime.noel@epitech.eu>
2023-05-20 16:57:10 +0200

    feat: reduce the blue bar on element

--- frontend/src/components/LeftBar/LeftBar.tsx
+++ frontend/src/components/LeftBar/LeftBar.tsx
@@ -7,12 +7,12 @@ const LeftBar: React.FC<LeftBarProps> = () => {
   return (
     <div className="h-full bg-white-300 text-white">
       <div className="flex" onClick={() => setiIconselected('document')}>
-        {iconselected == 'document' && <div className="bg-osmium-primary h-100 w-2"></div>}
+        <div className={(iconselected == 'document' ? "bg-osmium-primary " : "") + "h-100 w-1"}></div>
         <Icon icon={'document'} size={64} stroke="#999999" viewBox="0 0 24 24" fill="none"></Icon>
       </div>
 
       <div className="flex" onClick={() => setiIconselected('zoom')}>
-        {iconselected == 'zoom' && <div className="bg-osmium-primary h-100 w-2"></div>}
+        <div className={(iconselected == 'zoom' ? "bg-osmium-primary " : "") + "h-100 w-1"}></div>
         <Icon icon={'zoom'} size={64} fill="#999999" viewBox="0 0 24 24"></Icon>
       </div>
     </div>
