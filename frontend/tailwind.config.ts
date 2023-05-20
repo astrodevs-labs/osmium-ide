a24c00f2563e6262b2a8f4300b52c2e774f514b3
leon <maxime.noel@epitech.eu>
2023-05-20 17:35:32 +0200

    chore: add osmium-color in tailwind.config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,8 +7,17 @@ export default {
   theme: {
     extend: {
       colors: {
+        'osmium-bg': '#161C22',
+        'osmium-bgLight': '#3E4E5C',
+        'osmium-bgCode': '#1A1E25',
+        'osmium-primary': '#3C9DA1',
+        'osmium-secondary': '#183454',
+        'osmium-border': '#151A1F',
+        'osmium-text': '#E6E6E6',
+        'osmium-warning': '#C58200',
+        'osmium-error': '#8E0000',
+        'osmium-sucess': '#009E10',
         'osmium-grey': '#999999',
-        'osmium-primary': '#3C9DA1'
       },},
   },
   plugins: [],
