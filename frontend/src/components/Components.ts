8c60f068b273d66cf941a741ca965257d9799e57
leon <maxime.noel@epitech.eu>
2023-05-20 16:46:22 +0200

    feat: add switch

--- frontend/src/components/Components.ts
+++ frontend/src/components/Components.ts
@@ -5,5 +5,6 @@ import Toolbar from './Toolbar/Toolbar';
 import Icon from './Icon/Icon';
 import Folder from './Folder/Folder';
 import LeftBar from './LeftBar/LeftBar';
+import Switch from './Switch/Switch';
 
-export { Button, CheckBox, Label, Toolbar, Icon, Folder, LeftBar };
+export { Button, CheckBox, Label, Toolbar, Icon, Folder, LeftBar, Switch };
