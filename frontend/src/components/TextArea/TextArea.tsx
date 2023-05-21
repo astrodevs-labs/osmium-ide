acafb32448610c5b2bb8d4f2e0aca62eeb817b78
leon <maxime.noel@epitech.eu>
2023-05-21 14:37:35 +0200

    fix: can't resize textArea anymore

--- frontend/src/components/TextArea/TextArea.tsx
+++ frontend/src/components/TextArea/TextArea.tsx
@@ -5,7 +5,7 @@ const TextArea: React.FC<TextAreaProps> = ({ content, setContent }) => {
   return (
     <textarea
       onChange={(e) => { setContent(e.target.value) }}
-      className="w-full h-full bg-gray-800 text-white font-mono text-sm p-2" >
+      className="resize-none w-full h-full bg-gray-800 text-white font-mono text-sm p-2" >
       {content}
     </textarea>
   );
