a8c1c7ff6f8ec964781559febf62f3e85d196da9
Théo Maringer <theo.maringer@epitech.eu>
2023-06-02 18:09:16 +0200

    feat(front/components/builder): add canvas, root and hierachy component

--- frontend/src/components/Sidebar/SidebarPanel.tsx
+++ frontend/src/components/Sidebar/SidebarPanel.tsx
@@ -2,10 +2,11 @@ import React, { FC } from 'react';
 
 interface SidebarPanelProps {
   children?: React.ReactNode;
+  expanded: boolean;
 }
 
-const SidebarPanel: FC<SidebarPanelProps> = ({ children }) => {
-  return <div>{children}</div>;
+const SidebarPanel: FC<SidebarPanelProps> = ({ children, expanded }) => {
+  return <div className="flex-col border-r border-osmium-bgLight p-2 min-w-fit">{expanded && children}</div>;
 };
 
 export default SidebarPanel;
