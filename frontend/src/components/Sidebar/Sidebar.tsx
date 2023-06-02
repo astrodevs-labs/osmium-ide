2e36e7f4033818ad926f30da2d66718038f70570
Théo Maringer <theo.maringer@epitech.eu>
2023-06-02 17:04:17 +0200

    feat(front/components/builder): add sidebar panel

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -2,11 +2,26 @@ import Icon from '../Icon/Icon';
 import React, { FC } from 'react';
 
 interface SideBarProps {
-  children?: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const Sidebar: FC<SideBarProps> = ({ children }) => {
-  return <div className="h-full fixed flex-col">{children}</div>;
+  return (
+    <div className="h-full flex fixed bg-amber-400">
+      <div className="flex-col bg-osmium-error">
+        {children &&
+          children
+            // @ts-ignore
+            .filter((child) => child.props.node.type === 'sidebar-item')}
+      </div>
+      <div className="flex-col bg-blue-500">
+        {children &&
+          children
+            // @ts-ignore
+            .filter((child) => child.props.node.type === 'sidebar-panel')}
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
--- frontend/src/components/Sidebar/SidebarPanel.tsx
+++ frontend/src/components/Sidebar/SidebarPanel.tsx
@@ -1,3 +1,11 @@
-const SidebarPanel = () => {};
+import React, { FC } from 'react';
+
+interface SidebarPanelProps {
+  children?: React.ReactNode;
+}
+
+const SidebarPanel: FC<SidebarPanelProps> = ({ children }) => {
+  return <div>{children}</div>;
+};
 
 export default SidebarPanel;
