8edaa59d2c895759576c37820dae235b8fb60933
0xMemoryGrinder <alexjublot@gmail.com>
2023-06-07 22:51:24 +0200

    feat: added FrontCommunication module with server
    feat(backend/frontCommunication): added server base logic (no request handling yet)
    
    fix(backend/frontCommunication): updated module name after bad copy paste
    
    ci(backend): added submodule fetch for modules test
    
    ci(backend): added missing git submodule fetch
    
    fix(backend/core): compilation issue
    
    fix(backend/frontCommunication): compilation issue
    
    pls work
    
    Removed submodule backend/frontCommunication/lib/uWebSockets/uWebSockets

---
+++ backend/frontCommunication/lib/uWebSockets/FindZlib.cmake
@@ -0,0 +1,15 @@
+cmake_minimum_required(VERSION 3.17)
+
+include(FetchContent)
+message(STATUS "Fetching zlib")
+FetchContent_Declare(
+        z
+        GIT_REPOSITORY https://github.com/madler/zlib.git
+        GIT_TAG v1.2.13
+)
+FetchContent_MakeAvailable(z)
+message(STATUS "Fetched zlib")
+
+
+add_library(z INTERFACE)
+target_include_directories(z INTERFACE ${z_SOURCE_DIR} ${z_BINARY_DIR})
