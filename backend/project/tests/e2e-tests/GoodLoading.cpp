3ce7dbd3dead195ed69d3bd1a7f28d05af2b6e44
0xMemoryGrinder <35138272+0xMemoryGrinder@users.noreply.github.com>
2023-06-07 21:56:42 +0200

    merge:  #166 from feature/file-tree-generation/file-tree-generation/create-project-module-staging
    Create project-module

---
+++ backend/project/tests/e2e-tests/GoodLoading.cpp
@@ -0,0 +1,21 @@
+/*
+** project, 2023
+** GoodLoading.cpp by 0xMemoryGrinder
+*/
+
+#include <gtest/gtest.h>
+#include <filesystem>
+#include "osmium/module-tester/Tester.hpp"
+
+TEST(CorrectBehavior, CorrectBehavior)
+{
+#if _WIN32
+    std::string path = "./backend-project.dll";
+#else
+    std::string path = "./backend-project.so";
+#endif
+    EXPECT_NO_THROW({
+        std::shared_ptr<osmium::moduleTester::Tester> tester = osmium::moduleTester::Tester::create();
+        tester->loadModule(path);
+    });
+}
