946a96c35b17752c11d3abc63cd8a83c9fe5ef5f
Théo Maringer <theo.maringer@epitech.eu>
2023-06-08 15:23:35 +0200

    feat(backend): implemented empty hierarchy module

---
+++ backend/hierarchy/src/HierarchyModule.cpp
@@ -0,0 +1,3 @@
+#include "../include/HierarchyModule.hpp"
+
+hierarchy::HierarchyModule::HierarchyModule(std::shared_ptr<core::MessagingInterface> msg) : _msg(msg) {}
---
+++ backend/hierarchy/tests/e2e-tests/GoodLoading.cpp
@@ -0,0 +1,21 @@
+/*
+** editing, 2023
+** GoodLoading.cpp by 0xMemoryGrinder
+*/
+
+#include <gtest/gtest.h>
+#include <filesystem>
+#include "osmium/module-tester/Tester.hpp"
+
+TEST(CorrectBehavior, CorrectBehavior)
+{
+#if _WIN32
+std::string path = "./backend-hierarchy.dll";
+#else
+std::string path = "./backend-hierarchy.so";
+#endif
+EXPECT_NO_THROW({
+std::shared_ptr<osmium::moduleTester::Tester> tester = osmium::moduleTester::Tester::create();
+tester->loadModule(path);
+});
+}
