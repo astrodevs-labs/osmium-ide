eaddce723739beab28b7967c25ab14611bd24f1f
0xtekgrinder <72015889+0xtekgrinder@users.noreply.github.com>
2023-06-08 20:42:58 +0000

    feat(backend): ProjectFiles payload now have a File struct

--- backend/payloads/include/ProjectFiles.hpp
+++ backend/payloads/include/ProjectFiles.hpp
@@ -1,7 +1,18 @@
 #pragma once
 
+#include "File.hpp"
+
 class ProjectFiles {
+
+    private:
+        std::vector<File> _files;
+
     public:
         ProjectFiles();
         ~ProjectFiles() = default;
+
+    public:
+        const std::vector<File> &getFiles() const;
+
+        void setFiles(std::vector<File> files);
 };
