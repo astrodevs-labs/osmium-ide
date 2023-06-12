7454581d00c25cf9d4937911fe98369f7711d3cf
EnergyCube <dev@energycube.fr>
2023-06-12 21:27:56 +0200

    refactor(backend): added missing class name

--- backend/payloads/src/Component.cpp
+++ backend/payloads/src/Component.cpp
@@ -50,7 +50,7 @@ namespace payloads
         }
     }
 
-    std::shared_ptr<Component> createSimpleButtonComponent(uid id, std::string label)
+    std::shared_ptr<Component> Component::createSimpleButtonComponent(uid id, std::string label)
     {
         std::shared_ptr<Component> component = std::make_shared<Component>(id);
         component->addProp({"label", label, true});
